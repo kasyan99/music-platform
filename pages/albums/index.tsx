83007e6c366df1ddd778ab5f0c1f445598932f88
Andrey <kasyan201999@gmail.com>
2022-11-16 23:53:20 +0200

    feat: add albums page

---
+++ pages/albums/index.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "layouts/MainLayout"
+import React from "react"
+
+const AlbumsPage: React.FC = () => {
+   return (
+      <MainLayout title="Music platform - tracks">
+         <div>No albums</div>
+      </MainLayout>
+   )
+}
+
+export default AlbumsPage
